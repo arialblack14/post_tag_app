e05b7eca8db34ea49df914a0478947f1352936c1
arialblack14 <tsirasalexis@yahoo.gr>
2016-10-19 00:40:07 +0300

    add views for all posts methods

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,57 @@
 class PostsController < ApplicationController
+  before_action :find_post, only: [:edit, :update, :show, :destroy]
+
   def index
     @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save(post_params)
+      flash[:success] = "Successfully created post!"
+      redirect_to posts_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @post.update_attributes(post_params)
+      flash[:success] = "Successfully updated post!"
+      redirect_to post_path(@post)
+    else
+      flash[:error] = "Error updating post!"
+      render :edit
+    end
+  end
+
+  def show
+  end
+
+  def destroy
+    if @post.destroy
+      flash[:success] = "Successfully deleted post!"
+      redirect_to posts_path
+    else
+      flash[:error] = "Error deleting post!"
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
+
+  def find_post
+    @post = Post.find(params[:id])
   end
 end
