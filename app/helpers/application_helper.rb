64931d881d4df029360e920be66ea3ce1bc0b952
arialblack14 <tsirasalexis@yahoo.gr>
2016-10-18 23:35:19 +0300

    solve missing title issue

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  # Solve the problem of missing page title
+  def full_title(page_title)
+    base_title = "Blog App"
+    if page_title.empty?
+      base_title
+    else
+      "#{page_title} | #{base_title}"
+    end
+  end
 end
